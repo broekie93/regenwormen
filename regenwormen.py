e96f962e1e8e686f9c03a80046d5ddacfa70bf68
s2589036 <nielsje@planet.nl>
2020-06-16 00:36:25 +0200

    gameover goed geregeld, punten tellen, winnaar bepalen

--- regenwormen.py
+++ regenwormen.py
@@ -21,7 +21,6 @@ def countpoints(list):
 	return totalpoints
 
 def eindscore(score,totalchosendices):
-	#HIER OOK DE OGEN VAN DE WORP ZODAT JE KUNT VERGELIJKEN OF JE ALLEEN MAAR DINGEN HEBT GEGOOID DIE JE AL GEKOZEN HEBT
 	if score < 21:
 		print("Uw score is te laag, u bent af.")
 		return 0
@@ -31,6 +30,20 @@ def eindscore(score,totalchosendices):
 	else:
 		print("Uw eindscore is ", score)
 		return score
+
+def gameover(table, playerdict, player):
+	uppertile = 0
+	if len(playerdict[player]) > 0:
+		uppertile = playerdict[player][-1]
+		playerdict[player].remove(uppertile)
+		table.append(uppertile)
+		print(uppertile, " teruggelegd.")
+		table = sorted(table)
+	if(uppertile) != max(table):
+		print(table[-1], " omgedraaid")
+		table = table[:-1]
+	return (table, playerdict, player)
+
 ###################################################################################
 
 def onego(numberofdices,totalchosendices):
@@ -76,25 +89,41 @@ def completeturn(table,playerdict,player):
 	dicesleft = 8
 	totalchosendices = []
 	chosendices = "-"
+	available_opponent_dices = []
+	for opponent in playerdict:
+		if player != opponent:
+			try:
+				available_opponent_dices.append(playerdict[opponent][-1])
+			except:
+				pass
+
 	if dicesleft == 0:
 		if score < 21:
 			print("Uw score is te laag en u heeft geen dobbelstenen over, u bent af.")
-			return "gameover"
+			(table, playerdict, player) = gameover(table,playerdict,player)
 		elif score > 34:
 			print("Uw score is te hoog, u bent af.")
+			(table, playerdict, player) = gameover(table, playerdict, player)
 		elif "r" not in totalchosendices:
 			print("U heeft geen regenworm gegooid en u heeft geen dobbelstenen over, u bent af.")
-			return "gameover"
-		else:
-			printeindscore(score, totalchosendices)
+			(table, playerdict, player) = gameover(table, playerdict, player)
 
-	while dicesleft != 0: # and mychoice != "": MAAR HIER NIET BESCHIKBAAR
+		elif score not in table and score not in available_opponent_dices:
+			print("De gekozen tegel is niet beschikbaar, u bent af.")
+			(table, playerdict, player) = gameover(table, playerdict, player)
+
+		else:
+			pass
+
+	while dicesleft != 0:
 		if len(chosendices) == 0:
 			print("Uw score is ", score, "\n")
 		if chosendices != "":
-			if score > 20 and "r" in totalchosendices:
-				play = input("wilt u nog spelen? (j/n)")
-				if play == "j":
+			if score > 20 and "r" in totalchosendices and score in available_opponent_dices+table:
+				play = "-"
+				while play[0].lower() not in "jn":
+					play = input("wilt u stoppen? (j/n)")
+				if play[0].lower() == "n":
 					try:
 						(score, dicesleft, totalchosendices) = playturn(score,dicesleft,totalchosendices)
 					except:
@@ -110,26 +139,43 @@ def completeturn(table,playerdict,player):
 					dicesleft = 0
 
 	if eindscore(score,totalchosendices) == 0:
-		playerdict[player] = playerdict[player][:-1]
-		table = table[:-1]
+		(table, playerdict, player) = gameover(table,playerdict,player)
 	else:
 		if eindscore(score,totalchosendices) in table:
 			table.remove(score)
 			playerdict[player].append(score)
-		else:
+		elif eindscore(score, totalchosendices) in available_opponent_dices:
 			for opponent in playerdict:
-				if opponent != player and len(playerdict[opponent]) > 1:
-					if eindscore(score, totalchosendices) == playerdict[player][-1]:
-						playerdict[player].append(score)
-						playerdict[opponent].remove(score)
+				if opponent != player and eindscore(score, totalchosendices) == playerdict[opponent][-1]:
+					playerdict[player].append(score)
+					print(score, " toegevoegd aan stapel van", player)
+					playerdict[opponent].remove(score)
+					print(score, " weggehaald van stapel van", opponent)
+		else:
+			(table, playerdict, player) = gameover(table, playerdict, player)
 
 	return (table, playerdict)
 
 import itertools
 
+def countscore(tileslist):
+	scoredict = {21:1, 22:1,23:1,24:1,25:2,26:2,27:2,28:2,29:3,30:3,31:3,32:3,33:4,34:4,35:4,36:4}
+	score = 0
+	for item in tileslist:
+		score += scoredict[item]
+	return score
+
+def masked(list):
+	if len(list) > 0:
+		visible = list[-1]
+		maskedlist = ["X" for i in range(len(list) - 1)] + [visible]
+		return maskedlist
+	else:
+		return []
 def main():
 	playerdict = {}
 	table = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+	#table = [21,22,23,24]
 	name = "-"
 	while name != "":
 		name = input("Voer de namen in van de mensen die meedoen. ")
@@ -139,21 +185,27 @@ def main():
 		for player in itertools.cycle(playerdict.keys()):
 			print(player.capitalize()+" is aan de beurt")
 			(table, playerdict) = completeturn(table, playerdict, player)
-			print(table,playerdict)
+			print(table)
+			for player in playerdict:
+				print(player,":",masked(playerdict[player]))
 
+			if len(table) == 0:
+				print("Het spel is ten einde.")
+				winnaarpunten = -1
+				winnaar = "-"
+
+				for player in playerdict:
+					if countscore(playerdict[player]) > winnaarpunten:
+						winnaarpunten = countscore(playerdict[player])
+						winnaar = player
+
+				print(winnaar, " heeft gewonnen!!!")
+				
+				for player in playerdict:
+					print(player,": ", countscore(playerdict[player]), "punten")
+				break
 
 main()
 
-#MAAK VAN MAIN COMPLETETURN DIE EINDSCORE RETURNT EN MAAK NIEUWE MAIN DIE EERST VRAAGT HOEVEEL SPELEERS ER ZIJN, ZOVEEL LIJSTEN IN EEN DICTIONARY INITIALISEERT EN 1 LIJST (TABLE)
-#AVAILABLE_TILES = (FOR USER IN DICT: DICT[USER][-1]) + (TABLE)
-#IF EINDSCORE IN AVAILABLE_TILES: WIL JE DIE PAKKEN?, YES -> WEGHALEN UIT LIJST EN TOEVOEGEN AAN LIJST VAN PERSOON, IF LEN(TABLE) == 0: GAME FINISHED
-#MAAK COUNTPOINTS MODULE MET DICHT DIE VOOR IEDERE TEGEL T AANTAL WORMEN BEVAT
-
-
-
-
-
-
-
-
-
+#TODO:
+#tegel lager pakken als tegel niet beschikbaar is
